19a3425a235c079e4ce8d40691d2fceb3321cabe
Dominic Farquharson <farquharsondominic@gmail.com>
2017-09-14 11:32:34 -0400

    comment reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,15 @@
 import { combineReducers } from 'redux';
 import calculatorReducer from './calculatorReducer';
 
+/*
+  - combinereducers: turns object whose values are 
+    different reducing functions int oa single reducing function
+  - calls every child reducer and returns a single state object
+  - shape of state object matches key of the reducers that were passed  
+*/
 const calculator = combineReducers({
-  calculator: calculatorReducer
+  // results in key of calculator added to state tree  
+  calculator: calculatorReducer,
 });
 
 export default calculator;
